9899b47343f4554ab663db78bd6864a8e7ec9ac0
Allan <quheng@babytree-inc.com>
2019-05-02 15:39:35 +0800

    fix

--- setup/ConfigureiOS.rb
+++ setup/ConfigureiOS.rb
@@ -13,9 +13,7 @@ module Pod
 
     def perform
 
-      # keep_demo = configurator.ask_with_answers("Would you like to include a demo application with your library", ["Yes", "No"]).to_sym
-
-      # framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Specta", "Kiwi", "None"]).to_sym
+      keep_demo = "NO"
       # case framework
       #   when :specta
       #     configurator.add_pod_to_podfile "Specta"
